23318436763c6dbd5843ba1018f2083107377bb5
Saruar Hosen <saruarhosen525@gmail.com>
2025-08-06 01:35:17 +0600

    nav & footer has been created

--- assets/js/main.js
+++ assets/js/main.js
@@ -96,49 +96,15 @@
     $("body").removeClass("locked");
   });
 
-  var $offcanvasNav = $(".mobile-menu__main"),
-    $offcanvasNavSubMenu = $offcanvasNav.find(".sub-menu");
-  $offcanvasNavSubMenu
-    .parent()
-    .prepend(
-      '<span class="menu-expand"><svg fill="#ffffff" width="17" height="17" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><!--! Font Awesome Pro 6.4.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license (Commercial License) Copyright 2023 Fonticons, Inc. --><path d="M201.4 342.6c12.5 12.5 32.8 12.5 45.3 0l160-160c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L224 274.7 86.6 137.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3l160 160z"/></svg></span>'
-    );
-
-  $offcanvasNavSubMenu.slideUp();
-
-  $offcanvasNav.on("click", "li a, li .menu-expand", function (e) {
-    var $this = $(this);
-    if (
-      $this
-        .parent()
-        .attr("class")
-        .match(/\b(dropdown|has-children|has-sub-menu)\b/) &&
-      ($this.attr("href") === "#" || $this.hasClass("menu-expand"))
-    ) {
-      e.preventDefault();
-      if ($this.siblings("ul:visible").length) {
-        $this.siblings("ul").slideUp("slow");
-      } else {
-        $this.closest("li").siblings("li").find("ul:visible").slideUp("slow");
-        $this.siblings("ul").slideDown("slow");
-      }
-    }
-    if (
-      $this.is("a") ||
-      $this.is("span") ||
-      $this.attr("clas").match(/\b(menu-expand)\b/)
-    ) {
-      $this.parent().toggleClass("menu-open");
-    } else if (
-      $this.is("li") &&
-      $this.attr("class").match(/\b('dropdown')\b/)
-    ) {
-      $this.toggleClass("menu-open");
-    }
-  });
-
-  // aos animation
-  AOS.init();
+  if ($(".wow").length) {
+    var wow = new WOW({
+      boxClass: "wow", // animated element css class (default is wow)
+      animateClass: "animated", // animation css class (default is animated)
+      mobile: true, // trigger animations on mobile devices (default is true)
+      live: true, // act on asynchronously loaded content (default is true)
+    });
+    wow.init();
+  }
 
   // odometer
   if ($(".odometer").length) {
